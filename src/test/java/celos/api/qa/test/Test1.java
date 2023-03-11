a779072fa7dd1806e44df57eb54ab327fc7a6822
subramanyamkongani <subramanyamkongani@localhost>
2023-03-11 23:25:41 +0530

    first commit

---
+++ src/test/java/celos/api/qa/test/Test1.java
@@ -0,0 +1,11 @@
+package celos.api.qa.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Project");
+
+	}
+
+}
